402a88143051f38bbd4ca76f75e9998df755723e
imencia <isaac.mencia@gmail.com>
2018-10-12 08:56:55 -0400

    Initial commit

---
+++ SuperAventura/Engine/HealingPotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item
+    {        
+        public int AmmountToHeal { get; set; }
+
+        public HealingPotion(int id, string name, string namePlural, int ammountToHeal) : base(id, name, namePlural)
+        {
+            AmmountToHeal = ammountToHeal;
+        }
+    }
+}
